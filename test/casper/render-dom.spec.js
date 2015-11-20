22e72c7c6691b9b8f166457d4bd7404734f70702
livoras <livoras@163.com>
2015-11-21 01:41:36 +0800

    render nodes

--- test/casper/render-dom.spec.js
+++ test/casper/render-dom.spec.js
@@ -2,19 +2,33 @@
 
 var el = require('../../lib/element')
 
-casper.test.begin('run', 2, function (test) {
+casper.test.begin('run', 9, function (test) {
   casper.start('test/casper/index.html', function () {
     test.assertTitle('Page Test', 'Title matched')
   })
 
   casper.then(function () {
-    var root = el('div', [
+    var root = el('div', {
+      'data-index': '0',
+      'style': 'color: red'
+    }, [
       el('div'),
-      el('div'),
-      el('div'),
-      el('div')
+      el('span'),
+      el('p'),
+      el('ul', {style: 'font-size: 12px'}),
+      'Fuck you'
     ])
-    test.assertEqual(root.count, 4, 'Element\'count should be rigth')
+    var rootEl = root.render()
+
+    test.assertEqual(root.count, 5, 'Element\'count should be rigth')
+    test.assertEqual(rootEl.getAttribute('data-index'), '0', 'Should generate index 0')
+    test.assertEqual(rootEl.getAttribute('style'), 'color: red', 'Should generate style')
+    test.assertEqual(rootEl.style.color, 'red', 'Should have real color red')
+
+    test.assertEqual(rootEl.childNodes.length, 5, 'Children elements should be created, too')
+    test.assertEqual(rootEl.childNodes[0].tagName, 'DIV', 'Child\' tag name should be matched')
+    test.assertEqual(rootEl.childNodes[3].style.fontSize, '12px', 'Child\' properties should be generated')
+    test.assertEqual(rootEl.childNodes[4].textContent, 'Fuck you', 'Text nodes are also generated')
   })
 
   casper.run(function () {
