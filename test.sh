004bbf21f639d6e4cc340155335be4bcb3ce41ed
livoras <livoras@163.com>
2015-12-16 22:48:46 +0800

    remove casperjs, mocha + jsdom is enough

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-standard && casperjs test ./test/casper/*.spec.js && istanbul cover ./node_modules/mocha/bin/_mocha ./test/*.spec.js -- -R spec
+standard && istanbul cover ./node_modules/mocha/bin/_mocha ./test/*.spec.js -- -R spec
